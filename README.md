ac53a0da92e8d3c039b664f46709a9e84c3f8363
Huzaifa Tapal <huzaifa.tapal@medtelligent.com>
2014-08-23 12:49:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,155 @@
+# AngularJS Packery Directive 
+
+An [AngularJS](http://angularjs.org/) directive to work with David Desandro's [Packery](http://packery.metafizzy.co/).
+
+## Usage
+
+1. `bower install --save angular-packery`
+2. Add `wu.packery` to your application's module dependencies.
+2. Include dependencies in your HTML.
+3. Use the `packery` directive.
+
+## Example
+
+See the [homepage](http://passy.github.io/angular-packery) for a live example.
+
+```html
+<div packery>
+    <div class="packery-brick" ng-repeat="brick in bricks">
+        <img ng-src="{{ brick.src }}" alt="A packery brick">
+    </div>
+</div>
+```
+
+You have to include the `packery` attribute on the element holding the bricks.
+The bricks are registered at the directive through the `packery-brick` CSS
+classname.
+
+The directive uses [`imagesloaded`](https://github.com/desandro/imagesloaded) to
+determine when all images within the `packery-brick` have been loaded and adds
+the `loaded` CSS class to to the element, so you can add custom styles and
+prevent ghosting effects.
+
+## Attributes
+
+### `item-selector`
+
+(Default: `.packery-brick`)
+
+You can specify a different [item
+selector](http://packery.metafizzy.co/options.html#itemselector) through the
+`item-selector` attribute. You still need to use `packery-brick` either as class
+name or element attribute on sub-elements in order to register it to the
+directive.
+
+*Example:*
+
+```html
+<packery item-selector=".mybrick">
+    <div packery-brick class="mybrick">Unicorns</div>
+    <div packery-brick class="mybrick">Sparkles</div>
+</packery>
+```
+
+### `column-width`
+
+The `column-width` attribute allows you to override the [the width of a column
+of a horizontal grid](http://packery.metafizzy.co/options.html#columnwidth). If
+not set, Packery will use the outer width of the first element.
+
+*Example:*
+
+```html
+<packery column-width="200">
+    <div class="packery-brick">This will be 200px wide max.</div>
+</packery>
+```
+
+### `preserve-order`
+
+The `preserve-order` attributes disables the `imagesLoaded` logic and will
+instead display bricks by the order in the DOM instead of by the time they are
+loaded. Be aware that this can lead to visual glitches if the size of the
+elements isn't set at the time they are inserted.
+
+*Example:*
+
+```html
+<packery preserve-order>
+    <div class="packery-brick"><img src="..." alt="Will always be shown 1st"></div>
+    <div class="packery-brick"><img src="..." alt="Will always be shown 2nd"></div>
+</packery>
+```
+
+### `load-images`
+
+This attribute defaults to `true` and allows to disable the use of `imagesLoaded`
+altogether, so you don't have to include the dependency if your packery layout
+doesn't actually make use of images.
+
+*Example:*
+
+```html
+<packery load-images="false">
+    <div class="packery-brick"><p>Only text.</p></div>
+    <div class="packery-brick"><p>And nothing but text.</p></div>
+</packery>
+```
+
+### `reload-on-show`
+
+The `reload-on-show` attribute triggers a reload when the packery element (or an
+ancestor element) is shown after being hidden, useful when using `ng-show` or 
+`ng-hide`. Without this if the viewport is resized while the packery element is 
+hidden it may not render properly when shown again.
+
+*Example:*
+
+```html
+<packery reload-on-show ng-show="showList">
+    <div class="packery-brick">...</div>
+    <div class="packery-brick">...</div>
+</packery>
+```
+
+When `showList` changes from falsey to truthy `ctrl.reload` will be called.
+
+### `packery-options`
+
+You can provide [additional options](http://packery.metafizzy.co/options.html)
+as expression either as `packery` or `packery-options` attribute.
+
+*Example:*
+
+```html
+<packery packery-options="{ transitionDuration: '0.4s' }">
+</packery>
+```
+
+Equivalent to:
+
+```html
+<div packery="{ transitionDuration: '0.4s' }">
+</div>
+```
+
+## Credits
+
+The directive is based on
+[a StackOverflow question](http://stackoverflow.com/questions/16504151/packery-with-angularjs)
+answered by James Sharp.
+
+
+## Contributing
+
+Pull requests welcome. Only change files in `src` and don't bump any versions.
+Please respect the code style in place. Follow the
+[AngularJS commit guidelines](https://github.com/angular/angular.js/blob/master/CONTRIBUTING.md#commit-message-format).
+
+## License
+
+MIT
+
+
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/passy/angular-packery/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+
---
+++ src/angular-packery.js
@@ -0,0 +1,183 @@
+/*!
+ * angular-packery <%= pkg.version %>
+ * License: MIT
+ */
+(function () {
+  'use strict';
+
+  angular.module('wu.packery', [])
+    .controller('PackeryCtrl', function controller($scope, $element, $timeout) {
+      var bricks = {};
+      var schedule = [];
+      var destroyed = false;
+      var self = this;
+      var timeout = null;
+
+      this.preserveOrder = false;
+      this.loadImages = true;
+
+      this.schedulePackeryOnce = function schedulePackeryOnce() {
+        var args = arguments;
+        var found = schedule.filter(function filterFn(item) {
+          return item[0] === args[0];
+        }).length > 0;
+
+        if (!found) {
+          this.schedulePackery.apply(null, arguments);
+        }
+      };
+
+      // Make sure it's only executed once within a reasonable time-frame in
+      // case multiple elements are removed or added at once.
+      this.schedulePackery = function schedulePackery() {
+        if (timeout) {
+          $timeout.cancel(timeout);
+        }
+
+        schedule.push([].slice.call(arguments));
+
+        timeout = $timeout(function runPackery() {
+          if (destroyed) {
+            return;
+          }
+          schedule.forEach(function scheduleForEach(args) {
+            $element.packery.apply($element, args);
+          });
+          schedule = [];
+        }, 30);
+      };
+
+      function defaultLoaded($element) {
+        $element.addClass('loaded');
+      }
+
+      this.appendBrick = function appendBrick(element, id) {
+        if (destroyed) {
+          return;
+        }
+
+        function _append() {
+          if (Object.keys(bricks).length === 0) {
+            $element.packery('bindResize');
+          }
+          if (bricks[id] === undefined) {
+            // Keep track of added elements.
+            bricks[id] = true;
+            defaultLoaded(element);
+            $element.packery('appended', element, true);
+          }
+        }
+
+        function _layout() {
+          // I wanted to make this dynamic but ran into huuuge memory leaks
+          // that I couldn't fix. If you know how to dynamically add a
+          // callback so one could say <packery loaded="callback($element)">
+          // please submit a pull request!
+          self.schedulePackeryOnce('layout');
+        }
+
+        if (!self.loadImages){
+          _append();
+          _layout();
+        } else if (self.preserveOrder) {
+          _append();
+          element.imagesLoaded(_layout);
+        } else {
+          element.imagesLoaded(function imagesLoaded() {
+            _append();
+            _layout();
+          });
+        }
+      };
+
+      this.removeBrick = function removeBrick(id, element) {
+        if (destroyed) {
+          return;
+        }
+
+        delete bricks[id];
+        $element.packery('remove', element);
+        this.schedulePackeryOnce('layout');
+      };
+
+      this.destroy = function destroy() {
+        destroyed = true;
+
+        if ($element.data('packery')) {
+          // Gently uninitialize if still present
+          $element.packery('destroy');
+        }
+        $scope.$emit('packery.destroyed');
+
+        bricks = [];
+      };
+
+      this.reload = function reload() {
+        $element.packery();
+        $scope.$emit('packery.reloaded');
+      };
+
+
+    }).directive('packery', function packeryDirective() {
+      return {
+        restrict: 'AE',
+        controller: 'PackeryCtrl',
+        link: {
+          pre: function preLink(scope, element, attrs, ctrl) {
+            var attrOptions = scope.$eval(attrs.packery || attrs.packeryOptions);
+            var options = angular.extend({
+              itemSelector: attrs.itemSelector || '.packery-brick',
+              columnWidth: parseInt(attrs.columnWidth, 10) || attrs.columnWidth
+            }, attrOptions || {});
+            element.packery(options);
+            var loadImages = scope.$eval(attrs.loadImages);
+            ctrl.loadImages = loadImages !== false;
+            var preserveOrder = scope.$eval(attrs.preserveOrder);
+            ctrl.preserveOrder = (preserveOrder !== false && attrs.preserveOrder !== undefined);
+            var reloadOnShow = scope.$eval(attrs.reloadOnShow);
+            if (reloadOnShow !== false && attrs.reloadOnShow !== undefined) {
+              scope.$watch(function () {
+                return element.prop('offsetParent');
+              }, function (isVisible, wasVisible) {
+                if (isVisible && !wasVisible) {
+                  ctrl.reload();
+                }
+              });
+            }
+
+            scope.$emit('packery.created', element);
+            scope.$on('$destroy', ctrl.destroy);
+          }
+        }
+      };
+    }).directive('packeryBrick', function packeryBrickDirective() {
+      return {
+        restrict: 'AC',
+        require: '^packery',
+        scope: true,
+        link: {
+          pre: function preLink(scope, element, attrs, ctrl) {
+            var id = scope.$id, index;
+
+            ctrl.appendBrick(element, id);
+            element.on('$destroy', function () {
+              ctrl.removeBrick(id, element);
+            });
+
+            scope.$on('packery.reload', function () {
+              ctrl.schedulePackeryOnce('reloadItems');
+              ctrl.schedulePackeryOnce('layout');
+            });
+
+            scope.$watch('$index', function () {
+              if (index !== undefined && index !== scope.$index) {
+                ctrl.schedulePackeryOnce('reloadItems');
+                ctrl.schedulePackeryOnce('layout');
+              }
+              index = scope.$index;
+            });
+          }
+        }
+      };
+    });
+}());
